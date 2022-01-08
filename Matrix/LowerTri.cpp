0aaa5677db6382a25f5c13c255600c8fde92eae5
deadgame1 <adityashanker1@gmail.com>
2022-01-08 13:03:19 +0530

    Updates - Lower Triangular class, it is now a menu driven program

--- Matrix/LowerTri.cpp
+++ Matrix/LowerTri.cpp
@@ -26,7 +26,8 @@ class LowerTriangular{
         void set(int i, int j, int x);
 };
 
-void LowerTriangular::display(){
+void LowerTriangular::display()
+{
     for(int i=1;i<=dimension;i++){
         for(int j=1;j<=dimension;j++){
             if(i>=j) {
@@ -38,32 +39,79 @@ void LowerTriangular::display(){
         cout<<endl;
     }
 }
-void LowerTriangular::get(int i, int j){
+void LowerTriangular::get(int i, int j)
+{
     if(i>=j) {
         cout<<A[(i*(i-1))/2 + j-1];
     }else {
         cout<<"0";
     }
 }
-void LowerTriangular::set(int i, int j, int x){
+void LowerTriangular::set(int i, int j, int x)
+{
     if(i>=j) {
         A[(i*(i-1))/2 + j-1] = x;
     }
 }
 
 int main(){
-    LowerTriangular d(4);
-    d.set(1,1,1);
-    d.set(2,1,2);
-    d.set(2,2,4);
-    d.set(3,1,6);
-    d.set(3,2,8);
-    d.set(3,3,3);
-    d.set(4,1,5);
-    d.set(4,2,7);
-    d.set(4,3,9);
-    d.set(4,4,4);
-    d.display();
+    int choice=0;
+    int dimensionInput, row, column, elem;
+    cout<<"Please enter dimension of the matrix - "<<endl;
+    cin>>dimensionInput;
+    LowerTriangular usersMatrix(dimensionInput);
+    cout<<"Matrix created"<<endl;
+
+    do
+    {
+        //display the menu
+        cout<<"Lower Triangular Matrix"<<endl;
+        cout<<"1. Set"<<endl;
+        cout<<"2. Get"<<endl;
+        cout<<"3. Display"<<endl;
+        cout<<"4. Exit"<<endl;
+        cin>>choice;
+        cout<<endl;
+
+        switch (choice)
+        {
+        case 1: //set
+            cout<<"Please enter row and column where element needs to be inserted - "<<endl;
+            cin>>row;
+            cin>>column;
+            cout<<"Please enter the element to be inserted - "<<endl;
+            cin>>elem;
+            usersMatrix.set(row,column,elem);
+            cout<<endl;
+            break;
+        case 2: //get
+            cout<<"Please enter row and column to get the element at that index - "<<endl;
+            cin>>row;
+            cin>>column;
+            usersMatrix.get(row,column);
+            cout<<endl;
+            break;
+        case 3: //display
+            usersMatrix.display();
+            cout<<endl;
+            break;
+        default:
+
+            break;
+        }
+    } while (choice >=1 && choice <=3);
+    
+    // d.set(1,1,1);
+    // d.set(2,1,2);
+    // d.set(2,2,4);
+    // d.set(3,1,6);
+    // d.set(3,2,8);
+    // d.set(3,3,3);
+    // d.set(4,1,5);
+    // d.set(4,2,7);
+    // d.set(4,3,9);
+    // d.set(4,4,4);
+    // d.display();
 
     return 0;
 }
