9070ab37b2c4b09d975feef71e1769b2d1d21109
deadgame1 <adityashanker1@gmail.com>
2022-01-23 17:58:55 +0530

    Refactor - fn to create a circular list

--- LinkedList/circularList.cpp
+++ LinkedList/circularList.cpp
@@ -8,39 +8,48 @@ struct Node
     struct Node *next;
 };
 
-
-
-void createCircularList(int *A, int n, struct Node *cl)
+struct Node * createCircularList(int *A, int n, struct Node *cl)
 {
     int i;
-    struct Node *head = cl, *first = NULL, *tail = NULL;
+    struct Node *tail = NULL;
     if(cl->next != NULL){
-        cout<<"please supply empty HEAD node"<<endl;
+        cout<<"please supply empty first node"<<endl;
+        return NULL;
     }
 
     for(i=0;i<n;i++)
     {
-        struct Node *p = new Node;
-        p->data = A[i];
         if(i==0){
-            head->next = p;
-            tail = first = p;
-        }else if(i==n-1){
-            tail->next = p;
-            p->next = first;
+            cl->data = A[i];
+            cl->next = NULL;
+            tail = cl;
         }else{
+            struct Node *p = new Node;
+            p->data = A[i];
             tail->next = p;
             tail = p;
         }
     }
+    tail->next = cl;
+    return cl;
+}
+
+void displayCircularList(struct Node *head)
+{
+    struct Node *p = head;
+    do
+    {
+        cout<<p->data<<" ";
+        p = p->next;
+    } while (p != head);
+    cout<<endl;
 }
 
 int main()
 {
-    struct Node *circularList = new Node;
-    circularList->next = NULL;
+    struct Node *list = new Node;
     int A[5] = {3,5,7,9,10};
-    createCircularList(A,5,circularList);
+    struct Node *head = createCircularList(A,5,list);
 
     return 0;
 }
