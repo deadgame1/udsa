ef5c483df31bff1d3fe66c441cb5baaa32a3b1b2
deadgame1 <adityashanker1@gmail.com>
2022-01-26 00:27:46 +0530

    Adds - program to insert in a doubly linked list

--- LinkedList/doublyLinkedList.cpp
+++ LinkedList/doublyLinkedList.cpp
@@ -49,11 +49,57 @@ int length(struct Node* f)
     }
     return count;
 }
+
+void insert(struct Node* f, int position, int value)
+{
+    int i;
+    struct Node *t=new Node;
+
+    struct Node *p=f;
+    if(position < 0 || position > length(f))
+        return;
+
+    if(position==0)
+    {
+        if(f->data==-1)
+        {
+            f->data=value;
+            f->next=f->prev=NULL;
+        }
+        else
+        {
+            t->data=value;
+            t->next=f;
+            t->prev=NULL;
+            f->prev=t;
+            f=t;
+        }
+    }
+    else
+    {
+        for(i=0;i<position-1;i++)
+            p=p->next;
+        t->data=value;
+        t->prev=p;
+        t->next=p->next;
+        if(p->next)
+            p->next->prev=t;
+        p->next=t;
+    }
+}
+
 int main()
 {
-    struct Node *first = new Node;
+    struct Node *first=new Node;
+    first->data=-1;//first node is unitialized
+
     int A[6]={1,3,5,7,9,11};
-    first=intializeDoublyLinkedList(first,A,6);
+    //first=intializeDoublyLinkedList(first,A,6);
+    //cout<<length(first)<<endl;
+
+    insert(first,0,5);
+    insert(first,1,15);
+    insert(first,2,25);
     cout<<length(first)<<endl;
     displayList(first);
 
