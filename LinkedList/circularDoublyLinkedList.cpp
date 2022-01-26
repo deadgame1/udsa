b9686e8aad9c6f83e3f56b76dd5712bcc85b91c7
deadgame1 <adityashanker1@gmail.com>
2022-01-27 02:06:11 +0530

    Adds - program to insert in a circular doubly linked list

--- LinkedList/circularDoublyLinkedList.cpp
+++ LinkedList/circularDoublyLinkedList.cpp
@@ -55,12 +55,50 @@ int count(struct Node* head)
     return count;
 }
 
+/**
+ * @brief insert fn for circular doubly linked list
+ * please note that in contrast to doubly linked list
+ * doesnt matter where you insert 4 links are modified.
+ * whereas in doubly linked list inserting at position 0 only resulted in 3 links being modified.
+ * 
+ * @param head 
+ * @param pos 
+ * @param x 
+ */
+void insert(struct Node* head, int pos, int x)
+{
+    int i;
+    struct Node *p=head;
+    if(pos < 0 || pos > count(head))
+        return;
+    struct Node* t=new Node;
+    t->data=x;
+    if(pos==0)
+    {
+        t->prev=head->prev;
+        head->prev->next=t;
+        t->next=head;
+        head->prev=t;
+        first=t; //head=t is wrong doesnt modify the actual header so directly have to use global pointer
+    }
+    else
+    {
+        for(i=0;i<pos-1;i++)
+            p=p->next;
+        t->next=p->next;
+        t->prev=p;
+        p->next->prev=t;
+        p->next=t;
+    }
+}
 
 int main()
 {
     int A[5]={3,5,7,9,11};
-    //createList(A,5);
-    //display(first);
-    cout<<count(first)<<endl;
+    createList(A,5);
+    display(first); 
+    insert(first,5,14);
+    display(first);
+    //cout<<count(first)<<endl;
     return 0;
 }
