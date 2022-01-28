fc7d16476fdb059e15b472b84fe299cc56fa5e6e
deadgame1 <adityashanker1@gmail.com>
2022-01-29 00:05:34 +0530

    Adds - fn for stack push() and intialization of stack using push in main()

--- Stack/stackUsingArray.cpp
+++ Stack/stackUsingArray.cpp
@@ -9,11 +9,6 @@ struct Stack
     int *s;
 };
 
-void push(struct Stack *st, int value)
-{
-    
-}
-
 int isFull(struct Stack st)
 {
     if(st.top == st.size - 1)
@@ -22,13 +17,27 @@ int isFull(struct Stack st)
         return false;
 }
 
+void push(struct Stack *st, int value)
+{
+    if(!isFull(*st))
+        st->s[++st->top]=value;
+}
+
 int main()
 {
+    int i,x;
     struct Stack myStack;
     cout<<"Enter size";
     cin>>myStack.size;
     myStack.s = new int[myStack.size];
     myStack.top = -1;
+
+    for(i=0;i<myStack.size;i++)
+    {
+        cout<<"Enter Value no. "<<i+1<<" -";
+        cin>>x;
+        push(&myStack, x);
+    }
 
     return 0;
 }
