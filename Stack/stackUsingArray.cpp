205b351cea8c9be59c1c426095b5d68455721dc3
deadgame1 <adityashanker1@gmail.com>
2022-01-28 23:56:08 +0530

    Adds - program for stack using Array

---
+++ Stack/stackUsingArray.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+struct Stack
+{
+    int size;
+    int top;
+    int *s;
+};
+
+void push(struct Stack *st, int value)
+{
+    
+}
+
+int main()
+{
+    struct Stack myStack;
+    cout<<"Enter size";
+    cin>>myStack.size;
+    myStack.s = new int[myStack.size];
+    myStack.top = -1;
+
+    return 0;
+}
