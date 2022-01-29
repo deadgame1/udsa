bceecf985e117c3cf226a6911d9105be41a446c7
deadgame1 <adityashanker1@gmail.com>
2022-01-29 21:50:05 +0530

    Adds -a fn to check if parenthesis in a string match or not, using stack data structure

--- Stack/stackUsingLinkedList.cpp
+++ Stack/stackUsingLinkedList.cpp
@@ -61,7 +61,8 @@ void stackLL::push(int x)
     Node *p = new Node(x,this->top);
     if(p == NULL)
         cout<<"Stack overflow"<<endl;
-    this->top=p;
+    else
+        this->top=p;
 }
 
 int stackLL::pop()
@@ -115,18 +116,49 @@ int stackLL::peek(int position)
 
 }
 
+int checkParenthesis(stackLL stk, string stringToBeChecked)
+{
+    int i,x;
+    for(i=0;i<stringToBeChecked.length();i++)
+    {
+        if(stringToBeChecked[i] == '(')
+        {
+            stk.push(1);
+        }
+        else if(stringToBeChecked[i] == ')')
+        {
+            x = stk.pop();
+            if(x==-1)
+                return 0;
+        }
+    }
+
+    if(stk.isEmpty() == 1)
+        return 1;
+    else
+        return 0;        
+}
 int main()
 {
     stackLL myStack = stackLL();
-    myStack.push(10);
-    myStack.push(20);
-    myStack.push(30);
-    myStack.display();
+    //myStack.push(10);
+    //myStack.push(20);
+    //myStack.push(30);
+    //myStack.display();
+
+    string A;
+    cout<<"Enter expression"<<endl;
+    cin>>A;
+    int result = checkParenthesis(myStack,A);
+    if(result==1)
+        cout<<"Parenthesis match"<<endl;
+    else    
+        cout<<"Parenthesis dont match"<<endl;
 
     //cout<<myStack.pop()<<endl;
     //cout<<myStack.pop()<<endl;
     //cout<<myStack.pop()<<endl;
-    cout<<myStack.peek(2)<<endl;
+    //cout<<myStack.peek(2)<<endl;
 
     //myStack.display();
 
