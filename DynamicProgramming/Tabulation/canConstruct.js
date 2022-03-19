ae0d1d80e83c40d5f9221611f7f383ff456a712b
deadgame1 <adityashanker1@gmail.com>
2022-03-20 00:56:16 +0530

    Adds - canConstruct dp problem using tabulation

--- DynamicProgramming/Tabulation/canConstruct.js
+++ DynamicProgramming/Tabulation/canConstruct.js
@@ -6,3 +6,39 @@
     wordBank` array.
     You may reuse elements of `wordBank` as many times as needed.
  */
+function canConstruct(target, wordBank){
+    const table = Array(target.length + 1).fill(false);
+
+    table[0] = true;
+
+    for(let i=0;i<=target.length;i++){
+        if(table[i] === true){    
+            for(let word of wordBank){
+                if(target.slice(i, i+word.length) === word){
+                    table[i+word.length] = true;
+                }
+            }
+        }
+    }
+
+    return table[target.length];
+}
+
+/**
+ * Analysis - 
+ * Time - O(m*n*m) // m outer loop, n inner loop, another m for slicing a string ,max slice could be target
+ * Space - O(m) //
+ */
+
+console.log(canConstruct("abcdef",["ab","abc","abcd","ef","cd"])); //true
+console.log(canConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"])); // true
+console.log(canConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])); // false
+console.log(canConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"])); // true
+console.log(canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", [
+  "e",
+  "ee",
+  "eee",
+  "eeee",
+  "eeeee",
+  "eeeeee",
+])); // false
