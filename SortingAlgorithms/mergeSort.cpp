625b79ef799275c33f4898b158122b4698fe3382
deadgame1 <adityashanker1@gmail.com>
2022-05-03 09:33:17 +0530

    Adds - iterative and recursive version of merge sort

--- SortingAlgorithms/mergeSort.cpp
+++ SortingAlgorithms/mergeSort.cpp
@@ -16,7 +16,7 @@ void merge(int A[],int low, int mid, int high){
     int i,j,k;
     i=low;
     j=mid+1;
-    k=0;
+    k=low;
     int B[100];
 
     while(i<=mid && j<=high){
@@ -33,17 +33,46 @@ void merge(int A[],int low, int mid, int high){
     for(;j<=high;j++)
         B[k++]=A[j];
 
-    for(int x=low, y=0;x<=high,y<=high-low;x++,y++){
-        A[x] = B[y];
-        cout<<A[x]<<" ";
+    for(int i=low ;i<=high;i++){
+        A[i] = B[i];
+        //cout<<A[x]<<" ";
     }
-    cout<<endl;
+    //cout<<endl;
+}
+
+void iterativeMerge(int A[],int sizeOfList){
+    int p,low, mid, high;
+    for(p=2;p<=sizeOfList;p=p*2){
+        for(int i=0;i+p-1<sizeOfList;i=i+p){
+            low = i;
+            high = i+p-1;
+            mid = (low+high)/2;
+            merge(A,low,mid,high);
+        }
+    }
+    if(p/2 < sizeOfList)
+        merge(A,0,p/2-1,sizeOfList-1);
+}
+
+void recursiveMergeSort(int A[], int low, int high){
+    if(low < high){
+        int mid = low+high/2;
+        recursiveMergeSort(A,low,mid);
+        recursiveMergeSort(A,mid+1,high);
+        merge(A, low, mid, high);
+    }
+
 }
 
 int main(){
-    int A[8] = {3,5,7,9,2,4,6,8};
+    int A[9] = {1,3,5,7,9,2,4,6,8};
+    int n = sizeof(A)/sizeof(A[0]);
+    iterativeMerge(A,n);
 
-    merge(A,0,3,7);
+    for(int i=0;i<n;i++){
+        cout<<A[i]<<" ";
+    }
+    cout<<endl;
 
     return 0;
 }
