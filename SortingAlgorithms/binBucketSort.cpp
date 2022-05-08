6370343656cf49d3efcf86e1a270a8e93389161b
deadgame1 <adityashanker1@gmail.com>
2022-05-08 10:35:39 +0530

    Adds - bin bucket sort algo

---
+++ SortingAlgorithms/binBucketSort.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+
+using namespace std;
+
+class Node{
+public:
+    int key;
+    Node *next;
+    Node(int key, Node* next){
+        this->key = key;
+        this->next = next;
+    }
+};
+
+int findMax(int list[], int sizeOfList){
+    int max = list[0];
+    for(int i=1;i<sizeOfList;i++){
+        if(max<list[i])
+            max = list[i];
+    }
+
+    return max;
+}
+
+/**
+ * @brief inserting elements at end of list
+ * 
+ * @param n 
+ * @param auxArray 
+ * @param position 
+ */
+void insertNode(Node* n, Node* auxArray[], int position){
+    if(auxArray[position] == NULL)
+        auxArray[position] = n;
+    else{
+        Node* curr = auxArray[position];
+        while(curr->next != NULL){
+            curr = curr->next;
+        }
+        curr->next = n;
+    }
+}
+
+void binBucketSort(int list[], int sizeOfList){
+    int maxNumberInList = findMax(list,sizeOfList);
+    // = new Node[maxNumberInList+1]
+    Node **auxArray = new Node*[maxNumberInList+1];
+
+    for(int i=0;i<maxNumberInList+1;i++)
+        auxArray[i]=NULL;
+
+    for(int i=0;i<sizeOfList;i++){
+        Node* n = new Node(list[i],NULL);
+        insertNode(n,auxArray,list[i]);
+    }
+
+    for(int i=0;i<maxNumberInList+1;i++){
+        if(auxArray[i] == NULL)
+            continue;
+        else{
+            //pop out elements in FIFO fashion
+            do{
+                cout<<auxArray[i]->key<<" ";
+            }while(auxArray[i]->next != NULL);
+        }
+    }
+}
+
+int main(){
+    int A[] = {5,7,6,1,3,9,4,8,2};
+    int n = sizeof(A)/sizeof(A[0]);
+    binBucketSort(A,n);
+    
+    return 0;
+}
